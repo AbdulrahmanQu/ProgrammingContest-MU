3126b74bbd521d8d2252654c2b1f8560d7255969
abdulrahmanalqunaibit <a.qunaibit@gmail.com>
2019-05-16 22:09:32 +0300

    error with the loop

--- Mr. Programmer Contest 3 (3-1).py
+++ Mr. Programmer Contest 3 (3-1).py
@@ -1,33 +1,44 @@
-
-#اكتب برنامج يستقبل مجموعة من الاعداد  الصحيح N ثم يقوم بطباعة مجموع اول واخر خانة  من عدد N
-#0 < T <= 100
-#0 <= N <= 1000000
-#*المدخلات*
-#T عدد صحيح يساوي عدد حالات الاختبار
-#N الاعداد الصحيحة
-#*المخرجات*
-#    مجموع اول واخر خانة  من العدد N
-
-
 user = int(input("How many numbers ? :"))
 
 for i in range(user):
     
     n = int(input("Enter the number {} : ".format(i+1)))
 
-rev = 0
-fd = 0
-s = 0
+    rev = 0
+    fd = 0
+    s = 0
 
-ld = n % 10
+    ld = n % 10
 
 while n > 0:
     
     r = n % 10
     rev = rev * 10 + r
     n = int(n / 10)
+    fd = rev % 10
 
-fd = rev % 10
-s = fd + ld
+for i in range(user):
+    s = fd + ld
+    print("\nSum of first and last digit is of number {} is :".format(i+1), s)
 
-print("\nSum of first and last digit is of number {} is :".format(i+1), s)
+
+
+#the current output (only prints the last index):
+#
+#How many numbers ? :3
+#Enter the number 1 : 456
+#Enter the number 2 : 678
+#Enter the number 3 : 432
+#('\nSum of first and last digit is of number 1 is :', 6)
+#('\nSum of first and last digit is of number 2 is :', 6)
+#('\nSum of first and last digit is of number 3 is :', 6)
+#
+#the expected output:
+#
+#How many numbers ? :3
+#Enter the number 1 : 456
+#Enter the number 2 : 678
+#Enter the number 3 : 432
+#('\nSum of first and last digit is of number 1 is :', 10)
+#('\nSum of first and last digit is of number 2 is :', 14)
+#('\nSum of first and last digit is of number 3 is :', 6)
