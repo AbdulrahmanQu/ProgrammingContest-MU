69de5edaff5b29d2f561584921a9755f1a9af6cc
abdulrahmanalqunaibit <a.qunaibit@gmail.com>
2019-05-17 01:31:03 +0300

    adding readme file

--- Third Q/Mr. Programmer Contest 3 (3-1).py
+++ Third Q/Mr. Programmer Contest 3 (3-1).py
@@ -26,25 +26,3 @@ for i in range(user):
 
 for j in range(len(result)):
     print("\nSum of first and last digit is of number {} is :".format(j+1), result [j])
-
-
-
-#the current output (only prints the last index):
-#
-#How many numbers ? :3
-#Enter the number 1 : 456
-#Enter the number 2 : 678
-#Enter the number 3 : 432
-#('\nSum of first and last digit is of number 1 is :', 6)
-#('\nSum of first and last digit is of number 2 is :', 6)
-#('\nSum of first and last digit is of number 3 is :', 6)
-#
-#the expected output:
-#
-#How many numbers ? :3
-#Enter the number 1 : 456
-#Enter the number 2 : 678
-#Enter the number 3 : 432
-#('\nSum of first and last digit is of number 1 is :', 10)
-#('\nSum of first and last digit is of number 2 is :', 14)
-#('\nSum of first and last digit is of number 3 is :', 6)
