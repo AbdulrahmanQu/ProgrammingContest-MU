3a8442a9d871f98b8f2ce9ea083bdb4bc53a6437
abdulrahmanalqunaibit <a.qunaibit@gmail.com>
2019-05-17 01:25:46 +0300

    fixed the error by khaled sahari

--- Mr. Programmer Contest 3 (3-1).py
+++ Mr. Programmer Contest 3 (3-1).py
@@ -1,25 +1,31 @@
 user = int(input("How many numbers ? :"))
 
+result = []
 for i in range(user):
     
     n = int(input("Enter the number {} : ".format(i+1)))
-
+    
     rev = 0
     fd = 0
     s = 0
-
-    ld = n % 10
-
-while n > 0:
     
-    r = n % 10
-    rev = rev * 10 + r
-    n = int(n / 10)
-    fd = rev % 10
-
-for i in range(user):
+    ld = n % 10
+    
+    while n > 0:
+        
+        r = n % 10
+        rev = rev * 10 + r
+        n = int(n / 10)
+        fd = rev % 10
+    
+    
     s = fd + ld
-    print("\nSum of first and last digit is of number {} is :".format(i+1), s)
+    
+    result.append(s)
+
+
+for j in range(len(result)):
+    print("\nSum of first and last digit is of number {} is :".format(j+1), result [j])
 
 
 
